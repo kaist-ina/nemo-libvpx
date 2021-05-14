10d8caadb28087577af27b0792f4ed3aef68ca16
Hyunho Yeo <chaos5958@gmail.com>
2021-05-14 05:39:36 +0000

    Update

--- nemo_client_arm64.sh
+++ nemo_client_arm64.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./configure --enable-vp8 --enable-vp9 --enable-libyuv --target=arm64-android-gcc --enable-debug --disable-install-docs --log=yes --enable-internal-stats --disable-unit-tests --disable-docs --disable-tools --sdk-path={$HOME}/android-ndk-r14b --extra-cflags="-mfloat-abi=softfp -mfpu=neon -D__STDC_LIMIT_MACROS -Wno-extern-c-compat -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS" --extra-cxxflags="-D__STDC_LIMIT_MACROS -Wno-extern-c-compat" --disable-webm-io
+./configure --enable-vp8 --enable-vp9 --enable-libyuv --target=arm64-android-gcc --enable-debug --disable-install-docs --log=yes --enable-internal-stats --disable-unit-tests --disable-docs --disable-tools --sdk-path=/workspace/android-ndk-r14b --extra-cflags="-mfloat-abi=softfp -mfpu=neon -D__STDC_LIMIT_MACROS -Wno-extern-c-compat -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS" --extra-cxxflags="-D__STDC_LIMIT_MACROS -Wno-extern-c-compat" --disable-webm-io
 
